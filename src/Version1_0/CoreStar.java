a457540c064834a1483fc7e653d85469ff1931ac
matts6206 <matts6206@FHC-IC0026800.wrdsb.ca>
2016-01-25 08:18:03 -0500

    Added all framework code.

---
+++ src/Version1_0/CoreStar.java
@@ -0,0 +1,283 @@
+package Version1_0;
+
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import javax.sound.midi.*;
+import javax.sound.sampled.*;
+
+////ABOVE IS IMPORTS ONLY, NO OTHER CODE PLZ////////////////////////////////////
+
+public class CoreStar {
+    public static double nP1X, nP1Y, nP2X, nP2Y, nAILFX, nAILFY, nAIMFX, nAIMFY, nAIHFX, nAIHFY, nAIMBX, nAIMBY, nAICDX, nAICDY;
+    public static int nShipSelect;
+    public static void main(String[] args) {
+        while(true){
+            if(nShipSelect==1){
+                
+            }
+            else if(nShipSelect==2){
+                
+            }
+            else if(nShipSelect==3){
+                
+            }
+            else if(nShipSelect==4){
+                
+            }
+            else if(nShipSelect==5){
+                
+            }
+            else{
+                
+            }
+            System.out.println("P1-X: " + nP1X + " = P1-Y: " + nP1Y);
+            System.out.println("P2-X: " + nP2X + " = P2-Y: " + nP2Y);
+            System.out.println("AI-LF-X: ");
+            System.out.println("AI-MF-X: ");
+            System.out.println("AI-HF-X: ");
+            System.out.println("AI-MB-X: ");
+            System.out.println("AI-CD-X: ");
+            System.out.println("BOSS-CWS-MAIN-X: ");
+            System.out.println("BOSS-CWS-T1-X: ");
+            System.out.println("BOSS-CWS-T2-X: ");
+            System.out.println("BOSS-CWS-T3-X: ");
+            System.out.println("BOSS-CWS-T4-X: ");
+            System.out.println("BOSS-CWS-T5-X: ");
+            System.out.println("BOSS-CWS-T6-X: ");
+            System.out.println("BOSS-CWS-T7-X: ");
+            System.out.println("BOSS-CWS-T8-X: ");
+            System.out.println("BOSS-CWS-CLF1-X: ");
+            System.out.println("BOSS-CWS-CLF2-X: ");
+            System.out.println("-------------------------------------------------");
+        }
+    }
+}
+
+class Player1 {
+    static double nXVel, nYVel;
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+    public static void MystShip(){
+        
+    }
+}
+
+class Player2 {
+    static double nXVel, nYVel;
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+    public static void MystShip(){
+        
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////THIS IS THE AI DIVIDER-BELOW IS AI SHIPS-ABOVE IS PLAYERS AND GAME SETUP////
+////////////////////////////////////////////////////////////////////////////////
+
+class ArtIntell_L1 {//EASY AI
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+}
+
+class ArtIntell_L2 {//MEDIUM AI
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+}
+
+class ArtIntell_L3 {//HARD AI
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+}
+
+class ArtIntell_L4 {//EXTREME AI
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+}
+
+class ArtIntell_L5 {//MLG HARDCORE AI lol wow nyan doge
+    public static void LFighter(){
+        
+    }
+    public static void MFighter(){
+        
+    }
+    public static void HFighter(){
+        
+    }
+    public static void MBShip(){
+        
+    }
+    public static void CDShip(){
+        
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////BELOW IS BOSSES ONLY, NO MUNDANE AI SHIPS ALLOWED///lol XD puny ships ha////
+////////////////////////////////////////////////////////////////////////////////
+
+class FinalBoss_L1 {//EASY AI
+    public static void CapitalShip(){
+        
+    }
+    public static void OrbitalWPs(){
+        
+    }
+    public static void CapLFighter(){
+        
+    }
+}
+
+class FinalBoss_L2 {//MEDIUM AI
+    public static void CapitalShip(){
+        
+    }
+    public static void OrbitalWPs(){
+        
+    }
+    public static void CapLFighter(){
+        
+    }
+}
+
+class FinalBoss_L3 {//HARD AI
+    public static void CapitalShip(){
+        
+    }
+    public static void OrbitalWPs(){
+        
+    }
+    public static void CapLFighter(){
+        
+    }
+}
+
+class FinalBoss_L4 {//EXTREME AI
+    public static void CapitalShip(){
+        
+    }
+    public static void OrbitalWPs(){
+        
+    }
+    public static void CapLFighter(){
+        
+    }
+}
+
+class FinalBoss_L5 {//MLG HARDCORE AI lol wow nyan doge
+    public static void CapitalShip(){
+        
+    }
+    public static void OrbitalWPs(){
+        
+    }
+    public static void CapLFighter(){
+        
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////BELOW IS FILE IO - SAVEGAMES, QUICKLOADS, AND LOADING EXTERNAL FILES////////
+////////////////////////////////////////////////////////////////////////////////
+
+class loadSave {
+    public static void loadGame(){
+        
+    }
+    public static void saveGame(){
+        
+    }
+    public static void quickLoad(){
+        
+    }
+    public static void quickSave(){
+        
+    }
+    public static void deleteSave(){
+        
+    }
+    public static void loadExternalSave(){
+        
+    }
+}
